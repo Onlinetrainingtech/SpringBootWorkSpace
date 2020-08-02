e39b4adaceb92c131d8030723a3532e10cdbd7e6
OnlineTrainer <mohamedtechdata@gmail.com>
2020-08-02 06:31:32 +0530

    31-07-2020

---
+++ Spring-boot-example2Web/Spring-boot-example2Web/src/main/java/com/xyz/Springbootexample2Web/SpringBootExample2WebApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.Springbootexample2Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample2WebApplication.class, args);
+	}
+
+}
---
+++ Spring-boot-example2Web/Spring-boot-example2Web/src/test/java/com/xyz/Springbootexample2Web/SpringBootExample2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.Springbootexample2Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExample2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
