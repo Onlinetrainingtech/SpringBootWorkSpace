6d26afa164d1b2e53adc6a6e68025d51948ad383
OnlineTrainer <mohamedtechdata@gmail.com>
2021-02-18 08:30:39 +0530

    18-02-2021

---
+++ Spring-boot-batch4/Spring-boot-batch4/src/main/java/com/example/Springbootbatch4/SpringBootBatch4Application.java
@@ -0,0 +1,13 @@
+package com.example.Springbootbatch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatch4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatch4Application.class, args);
+	}
+
+}
---
+++ Spring-boot-batch4/Spring-boot-batch4/src/test/java/com/example/Springbootbatch4/SpringBootBatch4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbootbatch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatch4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
