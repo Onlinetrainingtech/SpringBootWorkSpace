63fc521b54310b68c605030aec9f498d17a4b3e6
OnlineTrainer <mohamedtechdata@gmail.com>
2021-02-19 09:04:07 +0530

    19-02-2021

---
+++ Spring-boot-h2-example-batch4/src/main/java/com/example/demo/SpringBootH2ExampleBatch4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2ExampleBatch4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2ExampleBatch4Application.class, args);
+	}
+
+}
---
+++ Spring-boot-rest-batch4/src/main/java/com/example/demo/SpringBootRestBatch4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestBatch4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestBatch4Application.class, args);
+	}
+
+}
