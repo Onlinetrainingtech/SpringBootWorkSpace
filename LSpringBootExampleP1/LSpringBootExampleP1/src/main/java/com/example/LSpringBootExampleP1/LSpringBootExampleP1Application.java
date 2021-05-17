4b4833e446705917d131c616143e391b12a2e841
OnlineTrainer <mohamedtechdata@gmail.com>
2021-05-17 08:28:18 +0530

    17-05-2021

---
+++ LSpringBootExampleP1/LSpringBootExampleP1/src/main/java/com/example/LSpringBootExampleP1/LSpringBootExampleP1Application.java
@@ -0,0 +1,13 @@
+package com.example.LSpringBootExampleP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LSpringBootExampleP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LSpringBootExampleP1Application.class, args);
+	}
+
+}
---
+++ LSpringBootExampleP2Web/LSpringBootExampleP2Web/src/main/java/com/example/LSpringBootExampleP2Web/LSpringBootExampleP2WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.LSpringBootExampleP2Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LSpringBootExampleP2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LSpringBootExampleP2WebApplication.class, args);
+	}
+
+}
