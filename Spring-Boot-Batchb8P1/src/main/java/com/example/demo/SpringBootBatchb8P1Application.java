08cc8240891f1127d1d60f407cdc0501ba449416
OnlineTrainer <mohamedtechdata@gmail.com>
2022-04-10 22:53:12 +0530

    10-04-2022

---
+++ Spring-Boot-Batchb8P1/src/main/java/com/example/demo/SpringBootBatchb8P1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchb8P1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchb8P1Application.class, args);
+	}
+
+}
---
+++ Spring-Boot-Batchb8Web/src/main/java/com/example/demo/SpringBootBatchb8WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchb8WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchb8WebApplication.class, args);
+	}
+
+}
