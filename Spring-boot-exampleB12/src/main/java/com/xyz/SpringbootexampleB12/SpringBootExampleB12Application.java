2427f5b61adb061c4d0531e5990eed37f6f070ef
OnlineTrainer <mohamedtechdata@gmail.com>
2022-09-21 07:48:30 +0530

    21-09-2022

---
+++ Spring-boot-exampleB12/src/main/java/com/xyz/SpringbootexampleB12/SpringBootExampleB12Application.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringbootexampleB12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleB12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleB12Application.class, args);
+	}
+
+}
---
+++ Spring-boot-exampleB12/src/test/java/com/xyz/SpringbootexampleB12/SpringBootExampleB12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.SpringbootexampleB12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleB12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
